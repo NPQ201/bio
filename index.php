c49e3565f5e0addc1192b1e4fbc1ecd820bc2a35
NPQ201 <36477650+NPQ201@users.noreply.github.com>
2021-07-02 22:32:14 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Profile Card</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+<link rel="stylesheet" href="./style.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="card">
+  <div class="banner">
+    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+      <image href="image.jpg" x="0" y="0" height="100" width="100"/>
+    </svg>
+  </div>
+  <div class="menu">
+  </div>
+  <h2 class="name">Đạt Đỗ</h2>
+  <div class="title">Designer, Developer, Editor</div>
+  <div class="actions">
+    <div class="follow-info">
+      <h2><a href="https://www.youtube.com/channel/UC7H1fR_0PsWO6D1npYOKFFg"><span>>5100</span><small>Followers</small></a></h2>
+      <h2><a href="#"><span>0</span><small>Following</small></a></h2>
+    </div>
+    <div class="follow-btn">
+      <button onclick="location.href='https://www.facebook.com/datdoreal/'" type="button"> 
+        Follow</button>  
+    </div>
+  </div>
+  <div class="desc">Đang có ý định làm một con game kinh dị.</div>
+</div>
+<!-- partial -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.10/vue.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js'></script>
+</body>
+</html>
